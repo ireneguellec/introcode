777253745c383b8accdf31069c5af9e7da4aeec3
IrèneGc <ireneguellec@gmail.com>
2018-04-10 11:45:22 +0200

    Java Script 3ème commit

--- cours 4/script.js
+++ cours 4/script.js
@@ -6,4 +6,17 @@ function welcome() {
 
 welcome();
 
+function getUsername() {
+    //affiche une boite de dialogue 
+    var  user = prompt("Veuillez rentrez votre nom:", "");
+    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
+    return user
+}; 
+
+function welcome() {
+    var name= getUsername();
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+
+};
+
 }); 
